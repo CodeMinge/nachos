2bb8721b8be4ba60cc74d313e04af9db3f550241
CodeMinge <825479572@qq.com>
2017-02-15 05:57:18 -0800

    speak_and_listen

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -1,5 +1,7 @@
 package nachos.threads;
 
+import java.util.LinkedList;
+
 import nachos.machine.*;
 
 /**
@@ -14,6 +16,9 @@ public class Communicator {
      * Allocate a new communicator.
      */
     public Communicator() {
+    	lock = new Lock();
+    	speaker = new Condition(lock);
+    	litener = new Condition(lock);
     }
 
     /**
@@ -26,7 +31,29 @@ public class Communicator {
      *
      * @param	word	the integer to transfer.
      */
+    /**
+     * 先获得锁，然后进行判断，如果没有听者等待，就要把说者的话e放入队列然后睡眠。
+     * 如果有听者等待，就要唤醒一个听者，然后传递消息，最后释放锁。
+     * @param word
+     */
     public void speak(int word) {
+    	boolean intStatus = Machine.interrupt().disable();
+    	lock.acquire();
+    	
+    	if(num_litener == 0) {
+    		num_speaker ++;
+    		queue.add(word);
+    		speaker.sleep();
+    		litener.wake();
+    		num_speaker --; 
+    	}
+    	else {
+    		queue.add(word);
+    		litener.wake();
+    	}
+    	
+    	lock.release();
+    	Machine.interrupt().restore(intStatus);
     }
 
     /**
@@ -35,7 +62,35 @@ public class Communicator {
      *
      * @return	the integer transferred.
      */    
+    /**
+     * 先获得锁，然后进行判断尝试唤醒speaker，如果没有说者等待，就要把听者放入队列然后睡眠。
+     * 如果有说者等待，就要唤醒一个说者，将自己挂起以等待speaker准备好数据再将自己唤醒，然后传递消息，最后释放锁。
+     * @return
+     */
     public int listen() {
-	return 0;
+    	boolean intStatus = Machine.interrupt().disable();
+    	lock.acquire();
+    	
+    	if(num_speaker == 0) {
+    		num_litener ++;
+    		litener.sleep();
+    		num_litener --; 
+    	}
+    	else {
+    		litener.sleep();
+    		speaker.wake();
+    	}
+    	
+    	lock.release();
+    	Machine.interrupt().restore(intStatus);
+    	
+    	return queue.poll();
     }
+    
+    private Lock lock; //互斥锁
+    Condition speaker;  //说者条件变量
+    Condition litener; //听者条件变量
+    private static int num_speaker = 0;
+    private static int num_litener = 0;
+    LinkedList<Integer> queue = new LinkedList<Integer>(); //说话者话语数量
 }
